91f2fb6070d1d145caef419f97c4a5b37aa0a323
mhetman <malgorzata.hetman@gmail.com>
2019-04-25 11:51:29 +0200

    uzupelnienie model sort result

--- src/sort-demo/Model.SortResults.pas
+++ src/sort-demo/Model.SortResults.pas
@@ -2,11 +2,41 @@ unit Model.SortResults;
 
 interface
 
+uses
+  System.TimeSpan, System.Diagnostics;
+
 type
   TSortResults = class
+  private
+    FSwapCounter: Integer;
+    FName: string;
+    FDataSize: Integer;
+    FStopWatch: TStopwatch;
+    FElapsed: TTimeSpan;
+  public
+    constructor Create(const AName: string; ADataSize: Integer);
+    procedure StartCountingTime;
+    procedure StopCountingTime;
 
+    property SwapCounter: Integer read FSwapCounter write FSwapCounter;
   end;
 
 implementation
+
+constructor TSortResults.Create(const AName: string; ADataSize: Integer);
+begin
+  FName := AName;
+  FDataSize := ADataSize;
+end;
+
+procedure TSortResults.StartCountingTime;
+begin
+  FStopWatch := TStopWatch.StartNew;
+end;
+
+procedure TSortResults.StopCountingTime;
+begin
+  FElapsed := FStopWatch.Elapsed;
+end;
 
 end.
