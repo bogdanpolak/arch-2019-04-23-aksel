8351caabd24955fbf7d8217fd0325a7fd23fc417
Pawel <pawel190x@gmail.com>
2019-04-25 11:36:54 +0200

    zwrocenie maksymalnej ilosci itemow w paintboxie

--- src/sort-demo/Controller.Board.pas
+++ src/sort-demo/Controller.Board.pas
@@ -8,12 +8,26 @@ uses
 type
   TBoardController = class
   private
-
+    FModel: TBoard;
+    FView: TBoardView;
   public
-
+    constructor CreateAndInit(AModel: TBoard; AView: TBoardView);
+    function GetItemsCount: Integer;
   end;
 
 implementation
 
+{ TBoardController }
+
+constructor TBoardController.CreateAndInit(AModel: TBoard; AView: TBoardView);
+begin
+  FModel := AModel;
+  FView := AView;
+end;
+
+function TBoardController.GetItemsCount: Integer;
+begin
+  Result := FView.ItemsCount;
+end;
 
 end.
