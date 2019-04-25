852184436fd3af14243c18e7e498a2c596664caa
Extermi111 <extermi111@gmail.com>
2019-04-25 12:10:13 +0200

    Dodanie klasy modelu TSortResults

--- src/sort-demo/Model.SortResults.pas
+++ src/sort-demo/Model.SortResults.pas
@@ -2,9 +2,21 @@ unit Model.SortResults;
 
 interface
 
+uses
+  System.TimeSpan;
+
 type
   TSortResults = class
-
+  private
+    FName: string;
+    FDataSize: Integer;
+    FEnlapsed: TTimeSpan;
+    FSwapCounter: Integer;
+  public
+    property Name: string read FName write FName;
+    property DataSize: Integer read FDataSize write FDataSize;
+    property Enlapsed: TTimeSpan read FEnlapsed write FEnlapsed;
+    property SwapCounter: Integer read FSwapCounter write FSwapCounter;
   end;
 
 implementation
