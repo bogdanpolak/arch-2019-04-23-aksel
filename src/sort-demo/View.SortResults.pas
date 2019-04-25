be89383c74701828049686374906768f1c927539
Extermi111 <extermi111@gmail.com>
2019-04-25 12:10:30 +0200

    Dodanie klasy widoku TSortResultsView

--- src/sort-demo/View.SortResults.pas
+++ src/sort-demo/View.SortResults.pas
@@ -2,11 +2,31 @@ unit View.SortResults;
 
 interface
 
+uses
+  Vcl.ExtCtrls, System.TimeSpan, Model.SortResults;
+
 type
   TSortResultsView = class
-
+  public
+    procedure DrawResults(paintbox: TPaintBox; AValue: TSortResults);
   end;
 
 implementation
+
+uses
+  Vcl.Graphics, System.SysUtils;
+
+procedure TSortResultsView.DrawResults(paintbox: TPaintBox; AValue: TSortResults);
+begin
+  paintbox.Canvas.Brush.Style := bsClear;
+  paintbox.Canvas.Font.Height := 18;
+  paintbox.Canvas.Font.Style := [fsBold];
+  paintbox.Canvas.TextOut(10, 5, AValue.Name);
+  paintbox.Canvas.Font.Style := [];
+  paintbox.Canvas.TextOut(10, 25, Format('items: %d', [AValue.DataSize]));
+  paintbox.Canvas.TextOut(10, 45, Format('time: %.3f',
+    [AValue.Enlapsed.TotalSeconds]));
+  paintbox.Canvas.TextOut(10, 65, Format('swaps: %d', [AValue.SwapCounter]));
+end;
 
 end.
