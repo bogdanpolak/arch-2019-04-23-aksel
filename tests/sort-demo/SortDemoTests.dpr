f72a22e3b7578727f482afade550d4724ed3ceed
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-05-01 00:01:01 +0200

    Dodano projekt testowy w DUnit

---
+++ tests/sort-demo/SortDemoTests.dpr
@@ -0,0 +1,26 @@
+﻿{ * ------------------------------------------------------------------------
+  * ♥  Akademia BSC © 2019  ♥
+  * Console Runner:
+  *   * Add "CONSOLE_TESTRUNNER" to Project -> Options -> Delphi Compiler -> Conditional defines
+  *   * http://docwiki.embarcadero.com/RADStudio/Rio/en/Delphi_Compiler
+  *  ----------------------------------------------------------------------- * }
+program SortDemoTests;
+
+{$IFDEF CONSOLE_TESTRUNNER}
+{$APPTYPE CONSOLE}
+{$ENDIF}
+
+uses
+  DUnitTestRunner,
+  Model.Board in '..\..\src\sort-demo\Model.Board.pas',
+  Controler.Sort in '..\..\src\sort-demo\Controler.Sort.pas',
+  Thread.Sort in '..\..\src\sort-demo\Thread.Sort.pas',
+  View.Board in '..\..\src\sort-demo\View.Board.pas',
+  TestBoard in 'TestBoard.pas';
+
+{$R *.RES}
+
+begin
+  DUnitTestRunner.RunRegisteredTests;
+end.
+
---
+++ tests/sort-demo/TestBoard.pas
@@ -0,0 +1,49 @@
+﻿{ * ------------------------------------------------------------------------
+  * ♥  Akademia BSC © 2019  ♥
+  *  ----------------------------------------------------------------------- * }
+unit TestBoard;
+
+interface
+
+uses
+  System.Generics.Collections, System.SysUtils, System.Classes,
+  TestFramework,
+  Model.Board;
+
+type
+  TestTBoard = class(TTestCase)
+  strict private
+    FBoard: TBoard;
+  private
+  public
+    procedure SetUp; override;
+    procedure TearDown; override;
+  published
+    procedure Test01;
+  end;
+
+implementation
+
+procedure TestTBoard.SetUp;
+begin
+  FBoard := TBoard.Create;
+end;
+
+procedure TestTBoard.TearDown;
+begin
+  FBoard.Free;
+  FBoard := nil;
+end;
+
+procedure TestTBoard.Test01;
+begin
+  FBoard.GenerateData(10);
+  CheckEquals(10, FBoard.Count, 'Nieodpowiednia liczba danych');
+end;
+
+
+initialization
+
+RegisterTest(TestTBoard.Suite);
+
+end.
