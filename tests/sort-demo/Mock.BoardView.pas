85a4aec2d963ea6f79f4262d26f0dcdeb71aa9b1
n.czaplicka <n.czaplicka@aksel.com.pl>
2019-05-07 09:55:32 +0200

    Implementacja TBoardViewMock

--- tests/sort-demo/Mock.BoardView.pas
+++ tests/sort-demo/Mock.BoardView.pas
@@ -11,14 +11,45 @@ uses
 
 // TODO: Zdefiniować klasę TBoardViewMock, która implementuje interface IBoardView
 (*
-   Uwaga!
-   -------
-   Metoda CountVisibleItems musi zwracać liczbę elementów. Nie warto wpisywać
-   za duzej wartości bo sortowanie bąbelkowe bedzie trwało zbyt długo/
-   Max. 20 elementów
+  Uwaga!
+  -------
+  Metoda CountVisibleItems musi zwracać liczbę elementów. Nie warto wpisywać
+  za duzej wartości bo sortowanie bąbelkowe bedzie trwało zbyt długo/
+  Max. 20 elementów
 *)
+
+type
+  TBoardViewMock = class(TInterfacedObject, IBoardView)
+  public
+    procedure DrawBoard;
+    procedure DrawItem(AIndex: Integer);
+    procedure DrawResults(const AAlgoritmName: string; AElapsedTime: TTimeSpan);
+    function CountVisibleItems: Integer;
+  end;
 
 implementation
 
+{ TBoardViewMock }
+
+function TBoardViewMock.CountVisibleItems: Integer;
+begin
+  Result := 15;
+end;
+
+procedure TBoardViewMock.DrawBoard;
+begin
+  { do nothing }
+end;
+
+procedure TBoardViewMock.DrawItem(AIndex: Integer);
+begin
+  { do nothing }
+end;
+
+procedure TBoardViewMock.DrawResults(const AAlgoritmName: string;
+  AElapsedTime: TTimeSpan);
+begin
+  { do nothing }
+end;
 
 end.
